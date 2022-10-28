e3d22455e57a13701e52eb40fb7b5af24311fab7
Bruna Oliveira <bruna.oliveira@programmers.com.br>
2022-10-28 15:05:38 -0300

    add Discount.API

---
+++ Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,45 @@
+﻿using Discount.API.Entities;
+using Discount.API.Entities.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public DiscountController(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository ?? throw new ArgumentNullException(nameof(discountRepository));
+        }
+
+        [HttpGet("{productName}", Name = "GetDiscount")]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var coupon = await _discountRepository.GetDiscount(productName);
+            return Ok(coupon);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody]Coupon coupon)
+        {
+            await _discountRepository.CreateDiscount(coupon);
+            return CreatedAtRoute("GetDicount", new {productName = coupon.ProductName});
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody]Coupon coupon)
+        {
+            return Ok(await _discountRepository.UpdateDiscount(coupon));
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<bool>> DeleteDiscount(string productName)
+        {
+            return Ok(await _discountRepository.DeleteDiscount(productName));
+        }
+    }
+}
---
+++ Discount.API/Entities/Repositories/IDiscountRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Discount.API.Entities.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon counpon);
+        Task<bool> UpdateDiscount(Coupon counpon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
